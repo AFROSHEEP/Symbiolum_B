2b5d4cfdbaefe2bce2a21ad90284023f3ea7d3ed
AFROSHEEP <srivathsan.prakash@gmail.com>
2019-09-06 15:59:40 -0600

    added smoothing to camera follow
    needs work

--- B_Prototype/Assets/CameraFollow.cs
+++ B_Prototype/Assets/CameraFollow.cs
@@ -3,11 +3,13 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 10f;
     public Vector3 offset;
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        transform.position = target.position + offset; 
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);// * Time.deltaTime);
+        transform.position = smoothedPos; 
     }
 }
