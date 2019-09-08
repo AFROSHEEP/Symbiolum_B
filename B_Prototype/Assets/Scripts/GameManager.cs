ceac882ac3d55e0acee9adc0dca513a364a176b5
AFROSHEEP <srivathsan.prakash@gmail.com>
2019-09-07 19:00:43 -0600

    minor change to gameManager

--- B_Prototype/Assets/Scripts/GameManager.cs
+++ B_Prototype/Assets/Scripts/GameManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public void possess()
+    {
+        //get animal in range
+        //player pos = animal pos and lock
+        //make it so can't possess again
+        //turn off wisp mesh renderer
+        //change movement variables
+        //enable animal skill
+    }
+
     // Start is called before the first frame update
     void Start()
     {
