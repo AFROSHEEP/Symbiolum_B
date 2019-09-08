c66eadc9266ea0e32f3d1c4b4192818d8af1a9d6
AFROSHEEP <srivathsan.prakash@gmail.com>
2019-09-07 18:55:24 -0600

    Added GameManager, started work on collisions

--- B_Prototype/Assets/Scripts/CameraFollow.cs
+++ B_Prototype/Assets/Scripts/CameraFollow.cs
@@ -3,13 +3,13 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 10f;
+    public float smoothSpeed;
     public Vector3 offset;
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
         Vector3 desiredPos = target.position + offset;
-        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);// * Time.deltaTime);
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPos; 
     }
 }
